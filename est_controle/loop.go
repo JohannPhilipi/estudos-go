bc160c19235ee203215305d1bedd4a3c1e23cabf
Johann Oliveira <joliveira.info@gmail.com>
2024-10-08 21:25:26 -0300

    FOR repeticao hierarquica

--- est_controle/loop.go
+++ est_controle/loop.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 
-	for x := 0; x < 10; x++ {
-		fmt.Println(x)
+	// for x := 0; x < 10; x++ {
+	// 	fmt.Println(x)
+	// }
+
+	for h := 0; h < 12; h++ {
+		fmt.Println("Hora:  ", h)
+		for min := 0; min < 60; min++ {
+			fmt.Println("Minuto:  ", min)
+		}
 	}
 }
