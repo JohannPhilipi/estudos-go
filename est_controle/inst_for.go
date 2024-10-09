a2be4d407a3ef7a3e9ac7009b17de332dc02b8fc
Johann Oliveira <joliveira.info@gmail.com>
2024-10-08 21:02:35 -0300

    instrucao IF

--- est_controle/inst_for.go
+++ est_controle/inst_for.go
@@ -4,6 +4,28 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		if i == 0 {
+			fmt.Println("Zero")
+		} else if i == 1 {
+			fmt.Println("Um")
+		} else if i == 2 {
+			fmt.Println("Dois")
+		} else if i == 3 {
+			fmt.Println("Tres")
+		} else if i == 4 {
+			fmt.Println("Quatro")
+		} else if i == 5 {
+			fmt.Println("Cinco")
+		} else if i == 6 {
+			fmt.Println("Seis")
+		} else if i == 7 {
+			fmt.Println("Sete")
+		} else if i == 8 {
+			fmt.Println("Oito")
+		} else if i == 9 {
+			fmt.Println("Nove")
+		} else if i == 10 {
+			fmt.Println("Dez")
+		}
 	}
 }
