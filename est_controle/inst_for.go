881fa500d1595374446c9eaab2dc89d93529b4ea
Johann Oliveira <joliveira.info@gmail.com>
2024-10-08 21:04:54 -0300

    instrucao SWITCH

--- est_controle/inst_for.go
+++ est_controle/inst_for.go
@@ -4,27 +4,28 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 10; i++ {
-		if i == 0 {
+		switch i {
+		case 0:
 			fmt.Println("Zero")
-		} else if i == 1 {
+		case 1:
 			fmt.Println("Um")
-		} else if i == 2 {
+		case 2:
 			fmt.Println("Dois")
-		} else if i == 3 {
+		case 3:
 			fmt.Println("Tres")
-		} else if i == 4 {
+		case 4:
 			fmt.Println("Quatro")
-		} else if i == 5 {
+		case 5:
 			fmt.Println("Cinco")
-		} else if i == 6 {
+		case 6:
 			fmt.Println("Seis")
-		} else if i == 7 {
+		case 7:
 			fmt.Println("Sete")
-		} else if i == 8 {
+		case 8:
 			fmt.Println("Oito")
-		} else if i == 9 {
+		case 9:
 			fmt.Println("Nove")
-		} else if i == 10 {
+		case 10:
 			fmt.Println("Dez")
 		}
 	}
