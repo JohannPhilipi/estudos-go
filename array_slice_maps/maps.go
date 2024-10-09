531fc561ee4fa97575bb9a06300486242edd50bd
Johann Oliveira <joliveira.info@gmail.com>
2024-10-08 22:46:38 -0300

    MAPS

---
+++ array_slice_maps/maps.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// map
+	// mp := make(map[string]int)
+	// mp["Idade"] = 30
+	// fmt.Println(mp["Idade"])
+
+	// map
+	mp := make(map[int]int)
+	mp[1] = 20
+	fmt.Println(mp[1])
+}
